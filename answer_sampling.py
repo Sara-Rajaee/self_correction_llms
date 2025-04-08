d7c38c060a736725d0964b2fe73d33703ec62b2f
baohaoliao <liaobaohao@gmail.com>
2025-04-08 20:18:42 +0200

    answer_sampling

--- answer_sampling.py
+++ answer_sampling.py
@@ -20,7 +20,7 @@ def parse_args():
     parser.add_argument("--model_name_or_path", default="gpt-4", type=str)
     parser.add_argument("--output_dir", default="./output", type=str)
     parser.add_argument("--split", default="test", type=str)
-    parser.add_argument("--num_test_sample", default=-1, type=int)  # -1 for full data
+    parser.add_argument("--num_test_sample", default=-1, type=int)
     parser.add_argument("--seed", default=0, type=int)
     parser.add_argument("--start", default=0, type=int)
     parser.add_argument("--end", default=-1, type=int)
